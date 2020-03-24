e0047d580555155d21d2d1472647b34c148cccaf
Viktor Makarov <gollwyd@mail.ru>
2020-03-25 01:06:52 +0300

    dom is done

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+document.addEventListener('scroll', onScroll);
+
+
+function onScroll (event) {
+    let curs = window.scrollY;
+    let links = document.querySelectorAll('.link');
+    let divs = document.querySelectorAll('.scroll');
+
+    divs.forEach((el)=> {
+        if(el.offsetTop-80 <= curs && (el.offsetTop + el.offsetHeight) > curs) {
+            links.forEach(a => {
+                a.classList.remove('active');
+                if(el.getAttribute('id') == a.getAttribute('href').substring(1)) {
+                    a.classList.add('active'); console.log('done');
+                }
+
+            });
+        }
+
+
+
+
+
+    });
+}
+
+
 
 document.querySelector('nav div').addEventListener('click', function(e) {
     
-    document.querySelectorAll('.link')[0].style.color = 'white';
-    document.querySelectorAll('.link')[1].style.color = 'white';
-    document.querySelectorAll('.link')[2].style.color = 'white';
-    document.querySelectorAll('.link')[3].style.color = 'white';
-    document.querySelectorAll('.link')[4].style.color = 'white';
-        e.target.style.color = '#f06c64';
+   
     e.preventDefault();
       
 
@@ -106,3 +128,81 @@ document.querySelector('.phone').addEventListener('click', function(e){
     }
     ++scL;
 });
+
+// табы
+let tags = document.querySelectorAll('.tags a');
+tags[0].style.color = 'white';
+document.querySelector('.tags').addEventListener('click', function(e) {
+    
+    tags[0].style.color = '#767e9e';
+    tags[1].style.color = '#767e9e';
+    tags[2].style.color = '#767e9e';
+    tags[3].style.color = '#767e9e';
+    
+        e.target.style.color = 'white';
+    //e.preventDefault();
+      
+
+});
+
+
+
+tags[0].addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector('.images').style.gridTemplateAreas = ' "a b c d" "e f h i" "j k l m" ';
+
+
+
+
+});
+tags[1].addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector('.images').style.gridTemplateAreas = ' "b a c d" "h i f e" "j k m l" ';
+
+});
+tags[2].addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector('.images').style.gridTemplateAreas = ' "b d c a" "h i f e" "j k l m" ';
+
+
+});
+tags[3].addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector('.images').style.gridTemplateAreas = ' "b a c d" "h i e f" "j l m k" ';
+
+
+});
+
+// граница
+
+let previousBorder = document.querySelector('.images');
+document.querySelector('.images').addEventListener('click', function(e){
+    if (e.target != document.querySelector('.images')) {
+    e.target.classList.add('border');
+    previousBorder.classList.remove('border');
+    previousBorder = e.target;
+    }
+});
+
+//Get a quote
+
+document.querySelector('#submit').addEventListener('click', function(e){
+    e.preventDefault();
+    let sub = document.querySelectorAll('.val')[2].value;
+    let des = document.querySelectorAll('.val')[3].value;
+    if (sub == "") sub = "Без темы";
+    if (des == "") des = "Без описания";
+    document.querySelector('.okno').style.display = 'block';
+    document.querySelector('#sub').innerText = sub;
+    document.querySelector('#des').innerText = des;
+});
+
+document.querySelector('#ok').addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector('.okno').style.display = 'none';
+    
+    document.querySelectorAll('.val').forEach(e => {
+        e.value = "";
+    });
+});
+
